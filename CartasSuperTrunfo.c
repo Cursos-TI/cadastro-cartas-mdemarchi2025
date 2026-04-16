8faa7c8213c2b3f8c30bdc2b1e340681a237fab8
MARCOS TADEU DEMARCHI <marcosdemarchi2012@gmail.com>
2026-04-16 21:44:07 +0000

    Apresentação do Jogo Super Trunfo Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -87,6 +87,7 @@ int main() {
     float calcular_pib_per_capitaC2 = (PibC2 * 1000000000) / PopulacaoC2;
 
     // Apresentação dos Atributos Calculados do Primeiro Jogador
+
     printf("==============================\n");
     printf("Estado: %s\n", EstadoC1);
     printf("Codigo da Carta: %s\n", Codigo_CartaC1);
@@ -99,6 +100,7 @@ int main() {
     printf("PIB per Capita: %.2f reais\n", calcular_pib_per_capitaC1);
 
     // Segundo Jogador
+
     printf("Carta do Segundo Jogador \n");
     printf("==============================\n");
     printf("Estado: %s\n", EstadoC2);
@@ -110,6 +112,79 @@ int main() {
     printf("Numero de Pontos Turisticos: %d\n", Num_Pontos_TuristicosC2);
     printf("Densidade Demográfica: %.2f hab/km²\n", densidade_demograficaC2);
     printf("PIB per Capita: %.2f reais\n", calcular_pib_per_capitaC2);
+
+    // Comparação dos Atributos para Determinar o Vencedor
+
+    printf("===============================\n");
+    printf("Comparando o Atributo População; \n");
+    printf("===============================\n");
+
+    if (PopulacaoC1 > PopulacaoC2) {
+        printf("População: Jogador 1 Vence!\n");
+    } else if (PopulacaoC1 < PopulacaoC2) {
+        printf("População: Jogador 2 Vence!\n");
+    } else {
+        printf("População: Empate!\n");
+    }
     
+    printf("===============================\n");
+    printf("Comparando o Atributo Área:) \n");
+    printf("===============================\n");
+
+    if (AreaC1 > AreaC2) {
+        printf("Área: Jogador 1 Vence!\n");
+    } else if (AreaC1 < AreaC2) {
+        printf("Área: Jogador 2 Vence!\n");
+    } else {
+        printf("Área: Empate!\n");
+    }
+
+    printf("===============================\n");
+    printf("Comparando o Atributo PIB: \n");
+    printf("===============================\n");
+
+    if (PibC1 > PibC2) {
+        printf("PIB; Jogador 1 Vence!\n");
+       } else if (PibC1 < PibC2) {
+        printf("PIB: Jogador 2 Vence!\n");
+    } else {
+        printf("PIB: Embate!\n");
+    }
+
+    printf("===============================\n");
+    printf("Comparando o Atributo Número de Pontos Turísticos: \n");
+    printf("===============================\n");
+
+    if (Num_Pontos_TuristicosC1 > Num_Pontos_TuristicosC2) {
+        printf("Numero de Pontos Turísticos: Jogador 1 Vence!\n");
+    } else if (Num_Pontos_TuristicosC1 < Num_Pontos_TuristicosC2) {
+        printf("Numero de Pontos Turísticos: Jogador 2 Vence!\n");
+    } else {
+        printf("Numero de Pontos Turísticos: Empate!\n");
+    }
+
+    printf("===============================\n");
+    printf("Comparando o Atributo Densidade Demográfica: \n");
+    printf("===============================\n");
+
+    if (densidade_demograficaC1 > densidade_demograficaC2) {
+        printf("Densidade Demográfica: Jogador 1 Vence!\n");
+    } else if (densidade_demograficaC1 < densidade_demograficaC2) {
+        printf("Densidade Demográfica: Jogador 2 Vence!\n");
+    } else {
+        printf("Densidade Demográfica: Empate!\n");
+    }
+
+    printf("===============================\n");
+    printf("Comparando o Atributo PIB per Capita: \n");
+    printf("===============================\n");
+
+    if (calcular_pib_per_capitaC1 > calcular_pib_per_capitaC2) {
+        printf("PIB per Capita: Jogador 1 Vence!\n");
+    } else if (calcular_pib_per_capitaC1 < calcular_pib_per_capitaC2) {
+        printf("PIB per Capita: Jogador 2 Vence!\n");
+    } else {
+        printf("PIB per Capita: Empate!\n");
+    }
 return 0;
 } 
